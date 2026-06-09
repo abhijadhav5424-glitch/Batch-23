817e3135b91d223086b4f3b3e5fa25a1c250d3a9
Abhijeet Jadhav <abhijadhav5424@gmail.com>
2026-06-09 14:03:00 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -6,7 +6,7 @@ resource "aws_instance" "myec2" {
     ami = "ami-07a00cf47dbbc844c"
     instance_type = "t3.micro"
     key_name = "id.pem"
-    vpc_security_group_ids = "default"
+    vpc_security_group_ids = ["sg-005b9e51ad6e35617"]
     tags = {
         Name = "myec2"
         name = "abhi"
